25082e9ab477510fddc56eb3e115add7ceadb79d
Luis Mota <38186@ufp.edu.pt>
2023-07-16 23:49:10 +0100

    -

--- EdgeDevice/NetworkService/Node_Test.py
+++ EdgeDevice/NetworkService/Node_Test.py
@@ -8,11 +8,12 @@ import ssl
 import uuid
 from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf, IPVersion, NonUniqueNameException
 from EdgeDevice.BlockchainService.Blockchain import Blockchain
-from EdgeDevice.NetworkService.MessageHandler import MessageHandler
 from EdgeDevice.NetworkService.NodeListener import NodeListener
+from EdgeDevice.BlockchainService.Transaction import validate_transaction
 from EdgeDevice.utils.constants import Network, HOST_PORT, BUFFER_SIZE, Messages
 import json
-from EdgeDevice.utils.helper import get_keys, get_tls_keys, get_interface_ip
+from EdgeDevice.utils.helper import get_keys, get_tls_keys, load_public_key_from_json, public_key_to_json, \
+    get_interface_ip, create_general_message
 
 logger = logging.getLogger(__name__)
 
@@ -70,7 +71,6 @@ class Node(threading.Thread):
             properties={'IP': self.ip, 'ID': self.id, 'LOCAL': self.local},
         )
         self.blockchain.register_node({self.ip: time.time()})
-        self.messageHandler = MessageHandler(self)
 
     def run(self):
         """
@@ -110,7 +110,7 @@ class Node(threading.Thread):
 
         try:
             print("[COORDINATOR] Starting the discovery service...")
-            browser = ServiceBrowser(self.zeroconf, "_node._tcp.local.", [self.listener.update_service])
+            ServiceBrowser(self.zeroconf, "_node._tcp.local.", [self.listener.update_service])
 
             threading.Thread(target=self.accept_connections).start()
         except KeyboardInterrupt:
@@ -181,31 +181,26 @@ class Node(threading.Thread):
 
         while self.running:
             try:
-                # Create a TCP socket
                 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 conn.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
-                # Wrap the socket with TLS encryption
                 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
                 context.check_hostname = False
                 context.verify_mode = ssl.CERT_NONE
                 conn = context.wrap_socket(conn, server_hostname=client_host)
 
-                # Connect to the peer using the TLS-encrypted socket
                 conn.connect((client_host, client_port))
                 conn.settimeout(self.keep_alive_timeout * 3)
 
-                # Continue with the rest of your code
                 self.add_node(conn, client_id, node_local)
                 self.list_peers()
 
                 handle_messages = threading.Thread(target=self.handle_messages, args=(conn,))
                 handle_messages.start()
 
-                handle_keep_alive_messages = threading.Thread(
-                    target=self.messageHandler.handle_keep_alive_messages(conn=conn, client_id=client_id),
-                    args=(conn, client_id))
+                handle_keep_alive_messages = threading.Thread(target=self.handle_keep_alive_messages,
+                                                              args=(conn, client_id))
                 handle_keep_alive_messages.start()
 
                 break
@@ -272,6 +267,110 @@ class Node(threading.Thread):
                 self.recon_state = False
                 continue
 
+    def handle_keep_alive_messages(self, conn, client_id):
+        """
+        The ``handle_keep_alive_messages`` method sends keep-alive messages to the specified connection periodically
+        to maintain the connection. The keep-alive message
+        includes information such as the sender's metadata, message type, last time alive, coordinator information, and
+        public key. If an exception occurs during the process, the function breaks the loop and closes the connection.
+
+        :param conn: Socket connection object representing the connection to the peer node.
+        :type conn: <socket.socket>
+        :param client_id: The unique identifier of the connected peer node.
+        :type client_id: <bytes>
+        :return: None
+        """
+        neighbour_id = uuid.UUID(client_id.decode('utf-8'))
+        neighbour = self.neighbours.get(neighbour_id)
+        while self.running:
+            try:
+                data = create_general_message(str(self.id), self.ip, self.port, conn.getpeername()[0],
+                                              conn.getpeername()[1],
+                                              str(neighbour_id), str(self.coordinator),
+                                              Messages.MESSAGE_TYPE_PING.value)
+
+                if neighbour is not None and neighbour['PUBLIC_KEY'] is None:
+                    data["PAYLOAD"]["PUBLIC_KEY"] = public_key_to_json(self.public_key)
+
+                time.sleep(2)
+                message = json.dumps(data, indent=2)
+                conn.send(bytes(message, encoding="utf-8"))
+                time.sleep(self.keep_alive_timeout)
+            except Exception as ex:  # Catch the specific exception you want to handle
+                logging.error(f"Exception error in Keep Alive: {ex.args}")
+                break
+
+        if conn in self.connections:
+            self.recon_state = True
+            self.remove_node(conn, "KAlive")
+            client_id_str = client_id.decode('utf-8')
+            client_uuid = uuid.UUID(client_id_str)
+            if client_uuid in self.neighbours:
+                self.neighbours.pop(client_uuid)
+            conn.close()
+
+    def handle_chain_message(self, message, conn, neighbour_id, message_type):
+        if message_type == Messages.MESSAGE_TYPE_GET_CHAIN.value:
+            if self.coordinator == uuid.UUID(message["PAYLOAD"].get("COORDINATOR")):
+                data = create_general_message(str(self.id), self.ip, self.port, conn.getpeername()[0],
+                                              conn.getpeername()[1],
+                                              str(neighbour_id), str(self.coordinator),
+                                              Messages.MESSAGE_TYPE_CHAIN_RESPONSE.value)
+
+                data["PAYLOAD"]["CHAIN"] = self.blockchain.chain
+
+                message_json = json.dumps(data, indent=2)
+                logging.info(f"CHAIN MESSAGE: {message_json}")
+                conn.send(bytes(message_json, encoding="utf-8"))
+        elif message_type == Messages.MESSAGE_TYPE_CHAIN_RESPONSE.value:
+            self.blockchain.chain = message["PAYLOAD"].get("CHAIN")
+            logging.info(f"IP: {self.ip} , CHAIN: {self.blockchain.chain}")
+            logging.info("Blockchain chain was updated with information from coordinator")
+
+    def handle_transaction_message(self, message, conn, neighbour_id):
+        tx = message["PAYLOAD"]["PENDING"]
+
+        if validate_transaction(tx):
+            if tx not in self.blockchain.pending_transactions:
+                self.blockchain.pending_transactions.append(tx)
+                data = create_general_message(str(self.id), self.ip, self.port, conn.getpeername()[0],
+                                              conn.getpeername()[1],
+                                              str(neighbour_id), str(self.coordinator),
+                                              Messages.MESSAGE_TYPE_TRANSACTION.value)
+
+                data["PAYLOAD"]["PENDING"] = self.blockchain.pending_transactions
+
+                message = json.dumps(data, indent=2)
+                conn.send(bytes(message, encoding="utf-8"))
+        else:
+            logging.warning("Received invalid transaction")
+            return
+
+    def handle_general_message(self, message, conn, neighbour_id):
+        message_type = Messages.MESSAGE_TYPE_PONG.value
+        if self.coordinator is None:
+            self.coordinator = uuid.UUID(message["PAYLOAD"].get("COORDINATOR"))
+            logging.info(f"Network Coordinator is {self.coordinator}")
+            message_type = Messages.MESSAGE_TYPE_GET_CHAIN.value
+
+        neighbour = self.neighbours.get(neighbour_id)
+        if neighbour is not None and neighbour['PUBLIC_KEY'] is None:
+            public_key_base64 = message['PAYLOAD']['PUBLIC_KEY']
+            public_key = load_public_key_from_json(public_key_base64)
+            if public_key is not None:
+                self.neighbours[neighbour_id]['PUBLIC_KEY'] = public_key
+
+        data = create_general_message(str(self.id), self.ip, self.port, conn.getpeername()[0],
+                                      conn.getpeername()[1],
+                                      str(neighbour_id), str(self.coordinator), message_type)
+
+        if neighbour is not None and neighbour['PUBLIC_KEY'] is None:
+            data["PAYLOAD"]["PUBLIC_KEY"] = public_key_to_json(self.public_key)
+
+        message_json = json.dumps(data, indent=2)
+        # logging.info(f"\nGENERAL MESSAGE: {message_json}\n")
+        conn.send(bytes(message_json, encoding="utf-8"))
+
     def handle_messages(self, conn):
         """
 
@@ -289,7 +388,7 @@ class Node(threading.Thread):
         while self.running:
             try:
                 data = conn.recv(BUFFER_SIZE).decode()
-                logging.info(f"Data found {data}")
+                # logging.info(f"Data found {data}")
                 if not data:
                     logging.info(f"Data not found {data}")
                     self.service_info.priority = random.randint(1, 100)
@@ -300,19 +399,19 @@ class Node(threading.Thread):
                 message_type = message.get("TYPE")
                 neighbour_id = uuid.UUID(message['META']['FROM_ADDRESS']['ID'])
 
-                logging.info(f"[MESSAGE TYPE]: {message_type}")
+                # logging.info(f"[MESSAGE TYPE]: {message_type}")
 
                 if message_type == Messages.MESSAGE_TYPE_PING.value:
-                    self.messageHandler.handle_general_message(message, conn, neighbour_id)
+                    self.handle_general_message(message, conn, neighbour_id)
 
                 elif message_type == Messages.MESSAGE_TYPE_GET_CHAIN.value:
-                    self.messageHandler.handle_chain_message(message, conn, neighbour_id, message_type)
+                    self.handle_chain_message(message, conn, neighbour_id, message_type)
 
                 elif message_type == Messages.MESSAGE_TYPE_TRANSACTION.value:
-                    self.messageHandler.handle_transaction_message(message, conn, neighbour_id)
+                    self.handle_transaction_message(message, conn, neighbour_id)
 
                 elif message_type == Messages.MESSAGE_TYPE_CHAIN_RESPONSE.value:
-                    self.messageHandler.handle_chain_message(message, conn, neighbour_id, message_type)
+                    self.handle_chain_message(message, conn, neighbour_id, message_type)
 
             except json.JSONDecodeError as e:
                 logging.error("Error decoding JSON:", e)
