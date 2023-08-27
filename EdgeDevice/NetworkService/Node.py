8b2474dd8edefae6508d648ef9ff5656f592791e
Luis Mota <38186@ufp.edu.pt>
2023-08-27 16:54:09 +0100

    -

--- EdgeDevice/NetworkService/Node.py
+++ EdgeDevice/NetworkService/Node.py
@@ -391,7 +391,7 @@ class Node(threading.Thread):
         try:
             if message_type == Messages.MESSAGE_TYPE_SEND_TRANSACTION.value:
 
-                message_tx = message["PAYLOAD"]["EVENT"]
+                message_tx = message["PAYLOAD"]["PENDING"]
 
                 tx, signature = create_transaction(self.private_key, self.public_key,
                                                    str(self.id),
