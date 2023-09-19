5963cef584eef49f751d390c8283a32575f7faa8
LuisMota1999 <38186@ufp.edu.pt>
2023-09-19 16:51:46 +0100

    Reconnect bug

--- EdgeDevice/NetworkService/Node.py
+++ EdgeDevice/NetworkService/Node.py
@@ -354,16 +354,13 @@ class Node(threading.Thread):
                 time.sleep(self.keep_alive_timeout * 2.5)
             except socket.error as e:
                 logging.error(f"Socket error: {e.args}")
-                if conn in self.connections:
-                    self.remove_node(conn, "KA")
-                    client_id_str = client_id.decode('utf-8')
-                    client_uuid = uuid.UUID(client_id_str)
-                    if client_uuid in self.neighbours:
-                        self.neighbours.pop(client_uuid)
-                    conn.close()
-                    handle_reconects = threading.Thread(target=self.handle_reconnects)
-                    handle_reconects.start()
-                    break
+                client_id_str = client_id.decode('utf-8')
+                client_uuid = uuid.UUID(client_id_str)
+                if client_uuid in self.neighbours:
+                    self.neighbours.pop(client_uuid)
+                handle_reconects = threading.Thread(target=self.handle_reconnects)
+                handle_reconects.start()
+                break
 
             except Exception as ex:
                 logging.error(f"Exception error in Keep Alive: {ex.args}")
