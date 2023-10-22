22b56066838487af8b8a2341271ca99fd806551b
Luis Mota <38186@ufp.edu.pt>
2023-10-22 21:06:39 +0100

    -

--- EdgeDevice/NetworkService/Node.py
+++ EdgeDevice/NetworkService/Node.py
@@ -291,26 +291,28 @@ class Node(threading.Thread):
         while self.running:
             inferred_audio_classes, top_score_audio = audio_inference.inference(waveform)
             inferred_video_classes, top_score_video = video_inference.inference(video_file_path)
-
-            if top_score_audio < audio_model['threshold'] or top_score_video < video_model['threshold'] or self.name == "NODE-2":
-                if len(self.blockchain.pending_transactions) > 0 and last_event_registered_bc != "":
-                    last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
-                        "INFERENCE", self.blockchain.pending_transactions)
-                    logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
-                    if last_event_registered_bc["EVENT_PRECISION"] >= top_score_video or last_event_registered_bc[
-                        "EVENT_PRECISION"] >= top_score_audio:
-                        self.process_detection(last_event_registered_bc["EVENT_PRECISION"], inferred_video_classes,
-                                               last_audio_class,
-                                               last_video_class, audio_inference, video_inference, top_score_audio,
-                                               top_score_video, True)
-                else:
-                    self.process_detection(inferred_audio_classes, inferred_video_classes, last_audio_class,
-                                           last_video_class, audio_inference, video_inference, top_score_audio,
-                                           top_score_video)
-            else:
-                self.process_detection(inferred_audio_classes, inferred_video_classes, last_audio_class,
-                                       last_video_class, audio_inference, video_inference, top_score_audio,
-                                       top_score_video)
+            last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
+                "INFERENCE", self.blockchain.pending_transactions)
+            logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
+            # if top_score_audio < audio_model['threshold'] or top_score_video < video_model['threshold'] or self.name == "NODE-2":
+            #     if len(self.blockchain.pending_transactions) > 0 and last_event_registered_bc != "":
+            #         last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
+            #             "INFERENCE", self.blockchain.pending_transactions)
+            #         logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
+            #         if last_event_registered_bc["EVENT_PRECISION"] >= top_score_video or last_event_registered_bc[
+            #             "EVENT_PRECISION"] >= top_score_audio:
+            #             self.process_detection(last_event_registered_bc["EVENT_PRECISION"], inferred_video_classes,
+            #                                    last_audio_class,
+            #                                    last_video_class, audio_inference, video_inference, top_score_audio,
+            #                                    top_score_video, True)
+            #     else:
+            #         self.process_detection(inferred_audio_classes, inferred_video_classes, last_audio_class,
+            #                                last_video_class, audio_inference, video_inference, top_score_audio,
+            #                                top_score_video)
+            # else:
+            #     self.process_detection(inferred_audio_classes, inferred_video_classes, last_audio_class,
+            #                            last_video_class, audio_inference, video_inference, top_score_audio,
+            #                            top_score_video)
 
             last_video_class = inferred_video_classes
             last_audio_class = inferred_audio_classes
