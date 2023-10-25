6f20345a927a85ff9b54ab4b95b588bb54341df2
Luis Mota <38186@ufp.edu.pt>
2023-10-25 21:31:21 +0100

    -

--- EdgeDevice/NetworkService/Node.py
+++ EdgeDevice/NetworkService/Node.py
@@ -53,7 +53,7 @@ class Node(threading.Thread):
         )
 
         node_number = int(self.name.split('-')[1].strip())
-        self.local = 'CASA DE BANHO' # if node_number % 2 == 0 else 'COZINHA' if node_number == 1 else 'QUARTO'
+        self.local = 'SALA' # if node_number % 2 == 0 else 'COZINHA' if node_number == 1 else 'QUARTO'
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(('0.0.0.0', self.port))
         self.socket.listen(5)
@@ -269,7 +269,7 @@ class Node(threading.Thread):
         video_model = {
             'model': Inference.VIDEO_MODEL.value,
             'resolution': 224,  # frame resolution
-            'threshold': 0.75  # confidence threshold for video classification
+            'threshold': 0.00  # confidence threshold for video classification
         }
 
         audio_inference = AudioInference(audio_model)
@@ -292,18 +292,20 @@ class Node(threading.Thread):
             inferred_audio_classes, top_score_audio = audio_inference.inference(waveform)
             inferred_video_classes, top_score_video = video_inference.inference(video_file_path)
 
-
+            last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
+                "INFERENCE", self.blockchain.pending_transactions)
+            logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
             if top_score_audio < audio_model['threshold'] or top_score_video < video_model['threshold'] or self.name == "NODE-2":
                 if len(self.blockchain.pending_transactions) > 0 and last_event_registered_bc != "":
                     last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
                         "INFERENCE", self.blockchain.pending_transactions)
                     logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
-                    # if last_event_registered_bc and last_event_registered_bc["EVENT_PRECISION"] >= top_score_video or last_event_registered_bc[
-                    #     "EVENT_PRECISION"] >= top_score_audio:
-                    #     self.process_detection(last_event_registered_bc["EVENT_PRECISION"], inferred_video_classes,
-                    #                            last_audio_class,
-                    #                            last_video_class, audio_inference, video_inference, top_score_audio,
-                    #                            top_score_video, True)
+                    if last_event_registered_bc["EVENT_PRECISION"] >= top_score_video or last_event_registered_bc[
+                        "EVENT_PRECISION"] >= top_score_audio:
+                        self.process_detection(last_event_registered_bc["EVENT_PRECISION"], inferred_video_classes,
+                                               last_audio_class,
+                                               last_video_class, audio_inference, video_inference, top_score_audio,
+                                               top_score_video, True)
                 else:
                     self.process_detection(inferred_audio_classes, inferred_video_classes, last_audio_class,
                                            last_video_class, audio_inference, video_inference, top_score_audio,
@@ -317,22 +319,33 @@ class Node(threading.Thread):
             last_audio_class = inferred_audio_classes
             time.sleep(2)
 
-
     def process_detection(self, inferred_audio_classes, inferred_video_classes, last_audio_class, last_video_class,
                           audio_inference, video_inference, top_score_audio, top_score_video, collaborative=False):
         inferred_classes = ""
         last_class = ""
         transaction_type = ""
+        if inferred_audio_classes != last_audio_class:
+            inferred_classes = inferred_audio_classes
+            last_class = last_audio_class
+            transaction_type = Transaction.TYPE_AUDIO_INFERENCE.value
+            logging.info(
+                f'[AUDIO - \'{audio_inference.model_name}\'] {inferred_audio_classes} ({top_score_audio})')
 
-        if inferred_video_classes != last_class:
+        if last_video_class != inferred_video_classes:
+            inferred_classes = inferred_video_classes
+            last_class = last_video_class
+            transaction_type = Transaction.TYPE_VIDEO_INFERENCE.value
+            logging.info(
+                f'[VIDEO - \'{video_inference.model_name}\'] {inferred_video_classes} ({top_score_video})')
 
-            transaction_with_signature = self.create_blockchain_transaction(inferred_audio_classes,
+        if inferred_video_classes != last_class or collaborative == True:
+
+            transaction_with_signature = self.create_blockchain_transaction(inferred_classes,
                                                                             'INFERENCE',
                                                                             self.local,
-                                                                            Transaction.TYPE_AUDIO_INFERENCE.value,
+                                                                            transaction_type,
                                                                             str(top_score_audio),
                                                                             )
-
 
             data = MessageHandlerUtils.create_transaction_message(
                 Messages.MESSAGE_TYPE_RESPONSE_TRANSACTION.value, str(self.id))
@@ -344,11 +357,6 @@ class Node(threading.Thread):
                                                                                   inferred_classes,
                                                                                   self.local)
             collaborative = False
-
-            last_event_registered_bc = NetworkUtils.get_last_event_blockchain(
-                "INFERENCE", self.blockchain.pending_transactions)
-            logging.info(f"Last Event Registered BC: {last_event_registered_bc}")
-
             if self.coordinator == self.id and self.coordinator != None:
                 self.homeassistant_listener.publish_message(homeassistant_data)
             self.broadcast_message(message)
