922d1b9f8e03956119a5895c847e38d99513a2ec
Luis Mota <38186@ufp.edu.pt>
2023-12-17 18:56:50 +0000

    -

--- EdgeDevice/main.py
+++ EdgeDevice/main.py
@@ -1,5 +1,5 @@
 import logging
-
+import time
 from EdgeDevice.NetworkService.Node import Node
 from EdgeDevice.utils import HOST_NAME
 from EdgeDevice.utils.helper import NetworkUtils
@@ -10,7 +10,7 @@ def main():
     NetworkUtils.generate_tls_keys()
 
     logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
-
+    time.sleep(2)
     node = Node(HOST_NAME)
 
     logging.info(f"Listening on {node.ip}:{node.port}...")
